a882a898e07100ed0a83ac913bc733d2f0ecf717
Jeet  Thakur <jeetthakur1607@gmail.com>
2025-02-04 02:39:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ hamburger.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   overlay.style.transform = 'translateX(-100%)';
 });
+
+
+/* bnt1 */
+
+function downloadPDF() {
+  const link = document.createElement("a");
+  link.href = "Resume.pdf";  // Path to your PDF file
+  link.download = "Jeet Thakur.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
