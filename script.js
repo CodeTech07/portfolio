964092a78c5376f39e3a3a7ba772292d8b05f8fb
Jeet  Thakur <jeetthakur1607@gmail.com>
2025-02-04 22:14:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -23,3 +23,51 @@ function downloadPDF() {
   link.click();
   document.body.removeChild(link);
 }
+
+
+/*Contact info*/
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("Kk0pQjXEDTUOpyg2l"); // Replace with your EmailJS Public Key
+
+  document.getElementById("contacts-form").addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      let name = document.getElementById("name").value;
+      let email = document.getElementById("email").value;
+    
+
+      let message = document.getElementById("message").value;
+
+      let templateParams = {
+          from_name: name,
+          from_email: email,
+        
+          message: message
+      };
+
+      emailjs.send("service_595miq6", "template_x9izn1l", templateParams)
+          .then(function (response) {
+              document.getElementById("response-msg").innerText = "Message sent successfully!";
+              document.getElementById("contacts-form").reset();
+          }, function (error) {
+              document.getElementById("response-msg").innerText = "Failed to send message. Try again.";
+              document.getElementById("response-msg").style.color = "red";
+          });
+  });
+});
+
+
+/* style */
+const sr = ScrollReveal ({
+  distance:'60px',
+  duration:2500,  
+  delay:400,
+  reset:true
+})
+
+sr .reveal ( '.portfolio-text',{delay:200, origin:'top'})
+sr .reveal ( '.portfolios-image',{delay:400, origin:'top'})
+
+sr .reveal ( '.social,.portfolio-image,.containers,.skills-card,.card,.container,.info',{delay:200, origin:'top'})
